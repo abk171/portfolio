f0bb522e850032af25e85ad29f732cfd06538aba
Abhigyan Kishor <ak1909552@qu.edu.qa>
2025-03-29 23:54:45 -0400

    completed work cards

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import './App.css'
 import StackCard from './components/stack_card';
 import ProjectCard, {ProjectCardProps} from './components/project_card';
 import ProjectCardRender from './components/project_card_render';
+import WorkCard, {WorkCardProps} from './components/Work_card';
 
 function App() {
     const myProject: ProjectCardProps = {
@@ -13,11 +14,25 @@ function App() {
         techStack: ["React", "OpenAI", "FastAPI"],
         githubLink: "http://github.com/AnuragJCChaturvedi/piar-backend",
     };
+
+    const myWork: WorkCardProps = {
+        workplace: "Pitt Health + Explainability AI Lab",
+        title: "Graduate Student Researcher",
+        location: "Pittsburgh, PA",
+        duration: "Jan 25 - Present",
+        link: "https://pitthexai.github.io/",
+        description: [
+            "Curated and cleaned a dataset of 200+ DICOM wrist ultrasound images from UPMC for NIH-funded carpal tunnel research",
+            "Developed a pipeline with YOLOv11 (mAP@50: 0.99), U-Net (IoU: 0.86), and a custom ConvNeXt classifier (94% accuracy, 0.86 precision, 1.0 recall)",
+            "Podium abstract submitted for AMIA 2025"
+        ]
+    }
     const myProjects:ProjectCardProps[] = Array(4).fill(myProject);
     return(
         <>
             {/* <ProjectCard {... myProject}/> */}
-            <ProjectCardRender projects={myProjects}/>
+            {/* <ProjectCardRender projects={myProjects}/> */}
+            <WorkCard {...myWork}/>
         </>
     )
 }
--- src/components/work_card.tsx
+++ src/components/work_card.tsx
@@ -4,9 +4,43 @@
 
 export type WorkCardProps = {
     workplace: string,
+    title: string,
+    location: string,
     duration: string,
     link: string,
     description: string[]
 }
 
-function WorkCard()
+function WorkCard({ workplace, title, location, duration, link, description }: WorkCardProps) {
+    return (
+        <>
+            <div className="shadow-lg p-4 rounded-xl bg-white transition-all duration-200 h-sm">
+                <div className="flex flex-col sm:flex-row justify-between">
+                    <a 
+                        className="text-2xl font-semibold text-black-600 underline hover:underline" 
+                        href={link} 
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                    >
+                        {workplace}
+                    </a>
+                    <div className="md:text-xl font-semibold mt-auto">{duration}</div>
+                </div>
+
+                <div className="flex flex-col sm:flex-row justify-between italic text-gray-600">
+                    <div>{title}</div>
+                    <div>{location}</div>
+                </div>
+
+                <ul className="list-disc list-outside pl-5 text-left pt-2 text-gray-500">
+                    {description.map((item, index) => (
+                        <li key={index}>{item}</li>
+                    ))}
+                </ul>
+            </div>
+        </>
+    );
+}
+
+
+export default WorkCard
