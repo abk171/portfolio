912cc98cd5cedb0f86275531e4eac348dc166d49
Abhigyan Kishor <ak1909552@qu.edu.qa>
2025-04-10 00:02:38 -0400

    about done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import myWorks from './assets/work.json'
-import myProjects from './assets/project.json'
+// import myWorks from './assets/work.json'
+// import myProjects from './assets/project.json'
 // Supports weights 100-700
 import NavBar from './components/navbar';
 import Footer from './components/footer';
@@ -11,32 +11,158 @@ import Home from './pages/home'
 import About from './pages/about'
 import Experience from './pages/experience';
 import Projects from './pages/projects';
+import { ProgramList } from './components/programs';
+import { WorkCardProps } from './components/work_card';
+import { ProjectCardProps } from './components/project_card';
+
+import { useEffect, useState } from 'react';
 
 
-const programLists = [
-    {
-        "name": "Languages",
-        "stack": ["Python", "Go", "Java", "R", "TypeScript"]
-    },
-    {
-        "name": "Web",
-        "stack": ["React", "Node", "Express", "FastAPI", "Django", "Tailwind"]
-    },
-    {
-        "name": "ML",
-        "stack": ["PyTorch", "TensorFlow", "HuggingFace", "Langchain"]
-    },
-    {
-        "name": "Database",
-        "stack": ["PostgreSQL", "MongoDB", "FAISS", "Neo4j"]
-    },
-    {
-        "name": "DevOps",
-        "stack": ["GCP", "AWS", "Docker", "Git Actions"]
-    }
-]
+
+
+
+// const programLists = [
+//     {
+//         "name": "Languages",
+//         "stack": ["Python", "Go", "Java", "R", "TypeScript"]
+//     },
+//     {
+//         "name": "Web",
+//         "stack": ["React", "Node", "Express", "FastAPI", "Django", "Tailwind", "Bootstrap"]
+//     },
+//     {
+//         "name": "ML",
+//         "stack": ["PyTorch", "TensorFlow", "Numpy", "Langchain", "OpenCV"]
+//     },
+//     {
+//         "name": "Database",
+//         "stack": ["PostgreSQL", "MongoDB", "Hadoop", "Redis"]
+//     },
+//     {
+//         "name": "DevOps",
+//         "stack": ["GCP", "AWS", "Docker", "Git Actions"]
+//     }
+// ]
+
+// const myWorks = [
+//     {
+//         "workplace": "Pitt HexAI Lab",
+//         "title": "Graduate Student Researcher",
+//         "location": "Pittsburgh, PA",
+//         "duration": "Jan 25 - Present",
+//         "link": "https://pitthexai.github.io/",
+//         "description": [
+//             "Curated and cleaned a dataset of 200+ DICOM wrist ultrasound images from UPMC for NIH-funded carpal tunnel research",
+//             "Developed a pipeline with YOLOv11 (mAP@50: 0.99), U-Net (IoU: 0.86), and a custom ConvNeXt classifier (94% accuracy, 0.86 precision, 1.0 recall)",
+//             "Podium abstract submitted for AMIA 2025"
+//         ]
+//     },
+
+//     {
+//         "workplace": "University of Pittsburgh,SCI",
+//         "title": "Graduate Teaching Assistant",
+//         "location": "Pittsburgh, PA",
+//         "duration": "Sep 24 - Present",
+//         "link": "https://www.sci.pitt.edu/academics/masters-degrees/data-science",
+//         "description": [
+//             "TA for Pitt’s Master’s Data Science course, assisting 150+ students in foundational math and statistics",
+//             "Review and audit 12 programming labs in nbgrader for testcase accuracy and Coursera integration"
+//         ]
+//     },
+
+//     {
+//         "workplace": "Hamad Medical Corporation",
+//         "title": "Software developer",
+//         "location": "Doha, Qatar",
+//         "duration": "Sep 22 - May 23",
+//         "link": "https://hamad.qa/EN/Education-and-research/Medical_Research/Pages/default.aspx",
+//         "description": [
+//             "Built a digital twin platform to replicate surgical environments for remote surgeon-led training and telementoring",
+//             "Stitched 3 AzureDK feeds in real time on LattePanda using RTX 4090 and CUDA-accelerated VTK for 3D rendering",
+//             "Reduced WebRTC bandwidth by 30%, improving framerate by up to 5 FPS in surgical telementoring software",
+//             "Recreated surgical scenes on Oculus with Unity, C#, and VTK; enabled bidirectional control via WebRTC data channels for interactive telementoring."
+//         ]
+//     },
+
+//     {
+//         "workplace": "Qatar Computing Research Center",
+//         "title": "Social computing intern",
+//         "location": "Doha, Qatar",
+//         "duration": "May 22 - Sep 22",
+//         "link": "https://www.hbku.edu.qa/en/qcri",
+//         "description": [
+//             "Won 2nd place for best summer internship project by building a large-scale data downloader for a flood detection pipeline",
+//             "Overcame GEE's 30MB download limit by dividing images into smaller tiles, enabling downloads up to 15GB in one call",
+//             "Built real-time ArcGIS + Redis dashboard used by 100+ responders during 2022 Pakistan floods"
+//         ]
+//     }
+// ]
+
+// const myProjects = [
+//     {
+//         "title": "Gosqlite (in progress)",
+//         "description": "A simple implementation of sqlite written in go.",
+//         "githubLink": "https://github.com/abk171/gosqlite"
+//     },
+//     {
+//         "title": "Model Implementations (in progress)",
+//         "description": "Full implementations of popular CV and NLP papers along with scripts for pretraining and testing; updated weekly",
+//         "githubLink": "https://github.com/abk171/model_implementations"
+//     },
+//     {
+//         "title": "Portfolio",
+//         "description": "This website :) React, Vite and tailwindcss.",
+//         "githubLink": "https://github.com/abk171/portfolio"
+//     },
+//     {
+//         "title": "MyYOLO",
+//         "description": "Dynamic tanh + ConvNext integration for YOLOv11, forked from ultralytics. Tested on MS COCO.",
+//         "githubLink": "https://github.com/abk171/ultralytics"
+//     },
+//     {
+//         "title": "InfinityGPT",
+//         "description": "Multimodal LLM for images, text and speech. Conducted fine-tuning and benchmarking on well known benchmarks.",
+//         "githubLink": "https://github.com/macabdul9/InfGPT"
+//     },
+
+    
+
+//     {
+//         "title": "First Patient Report",
+//         "description": "BioHacks 2024 submission for an automatic transcription service for EMTs to complete hospital handover forms.",
+//         "githubLink": "http://github.com/AnuragJCChaturvedi/piar-backend"
+//     },
+
+//     {
+//         "title": "Paint Statistics,PPG",
+//         "description": "Regression and classification of paint quality data in R.",
+//         "githubLink": "https://github.com/abk171/infsci-2595"
+//     },
+//     {
+//         "title": "Tooth x-ray dataset augmentation",
+//         "description": "CycleGAN generated tooth-xray and corresponding segmentation masks in PyTorch.",
+//         "githubLink": "https://github.com/abk171/Artificial-Neural-Networks/tree/main/projects"
+//     }
+// ]
 
 function App() {
+    const [programLists, setProgramLists] = useState<ProgramList[]>([]);
+    const [myWorks, setMyWorks] = useState<WorkCardProps[]>([]);
+    const [myProjects, setMyProjects] = useState<ProjectCardProps[]>([]);
+
+    useEffect(() => {
+    const loadData = async () => {
+        const programs = await (await fetch('/stack.json')).json();
+        const works = await (await fetch('/work.json')).json();
+        const projects = await (await fetch('/project.json')).json();
+
+        setProgramLists(programs);
+        setMyWorks(works);
+        setMyProjects(projects);
+    };
+
+loadData();
+}, []);
    
     return(
         <>
--- src/components/programs.tsx
+++ src/components/programs.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
-import { FaJava, FaPython, FaRProject } from 'react-icons/fa';
+import { FaAws, FaBootstrap, FaJava, FaNode, FaPython, FaReact, FaRProject } from 'react-icons/fa';
 import { FaGolang } from 'react-icons/fa6';
-import { SiTypescript } from 'react-icons/si';
+import { GrStatusUnknown } from 'react-icons/gr';
+import { MdKeyboardArrowDown } from 'react-icons/md';
+import { SiDjango, SiFastapi, SiNumpy, SiOpencv, SiPandas, SiTidyverse, SiTypescript , SiPytorch, SiTensorflow, SiLangchain, SiDocker, SiPostgresql, SiApachehadoop, SiMongodb, SiRedis, SiGooglecloud, SiGithubactions, SiExpress, SiTailwindcss} from 'react-icons/si';
 import { TbBrandCpp } from 'react-icons/tb';
 
 export type ProgramList = {
@@ -15,58 +17,90 @@ const techMap = {
     "C++": <TbBrandCpp/>,
     "TypeScript": <SiTypescript/>,
     "Go": <FaGolang/>,
-    "R": <FaRProject/>
+    "R": <FaRProject/>,
+    "React": <FaReact/>,
+    "Node": <FaNode/>,
+    "Express": <SiExpress/>,
+    "Tailwind": <SiTailwindcss/>,
+    "Django": <SiDjango/>,
+    "Bootstrap": <FaBootstrap/>,
+    "FastAPI": <SiFastapi/>,
+    "Pandas": <SiPandas/>,
+    "Numpy": <SiNumpy/>,
+    "TidyVerse": <SiTidyverse/>,
+    "OpenCV": <SiOpencv/>,
+    "PyTorch": <SiPytorch />,
+    "TensorFlow": <SiTensorflow/>,
+    "Langchain": <SiLangchain/>,
+    "Docker": <SiDocker/>,
+    "PostgreSQL": <SiPostgresql/>,
+    "Hadoop": <SiApachehadoop/>,
+    "MongoDB": <SiMongodb/>,
+    "Redis": <SiRedis/>,
+    "GCP": <SiGooglecloud/>,
+    "AWS":  <FaAws/>,
+    "Git Actions": <SiGithubactions/>
 }
 
+
+
 function Programs({plists}: {plists: ProgramList[]}) {
-    const [selectedCount, setSelectedCount] = useState(1);
+    const [selectedCount, setSelectedCount] = useState(2);
+    const [isDropdown, setDropdown] = useState(false);
 
     const handleClick = (count: number) => {
         setSelectedCount(count)
+    }
+
+    const handleDropdown = (open: boolean) => {
+        setDropdown(!open);
     }
 
     return(
         <div className="min-h-[100vh] flex flex-col">
             <h1 className="text-left text-xl text-bold pb-8 ">About</h1>
             <span className="text-sm pt-4 text-gray-500">
-                Currently, I am a graduate student at the <a>University of Pittsburgh</a>. 
-                I have also taken <i>Intro to deep learning</i> at <a>Carnegie Mellon University</a>, to gain a more comprehensive knowledge of the subject.
+                Currently, I am a graduate student at the <a href="https://www.pitt.edu/" target="_blank" rel="noopener noreferrer">University of Pittsburgh</a>. 
+                I have also taken <i>Intro to deep learning</i> at <a href='https://www.cmu.edu/' rel="noopener noreferrer" target="_blank">Carnegie Mellon University</a>, to gain a more comprehensive knowledge of the subject.
             </span>
             <span className="mt-2 text-sm text-gray-500">
                 Following are some of the technologies I am proficient in.
             </span>
 
-            {/* <div className="flex flex-row mt-4 text-gray-500 ">
-                {plists.map((plist, index) => {
-                    let buttonClass = "regular-button-style";
+            
+            <div className="flex flex-row item-start mt-4 lg:flex-col text-gray-500 text-sm lg:items-center" >
+                {/* Create the buttons here */}
+                <div className="flex flex-col lg:flex-row mt-4 ">
+                    {plists.map((plist, index) => {
+                        let baseButtonClass = "w-[140px] flex-none text-[#646cff] !bg-[#FFFFFF] hover:!shadow-xl ";
+                        let buttonClass = "";
 
-                    if (index === 0) {
-                        buttonClass = "!rounded-r-none";
-                    } else if (index === plists.length - 1) {
-                        buttonClass = "!rounded-l-none";
-                    } else {
-                        buttonClass = "!rounded-none";
-                    }
+                        if (index === 0) {
+                            buttonClass = "lg:!rounded-r-none";
+                        } else if (index === plists.length - 1) {
+                            buttonClass = "lg:!rounded-l-none";
+                        } else {
+                            buttonClass = "lg:!rounded-none";
+                        }
 
-                    return (
-                        <button key={index} className={buttonClass} onClick={() => handleClick(index)}>
-                            {plist.name}
-                        </button>
-                    );
-                })}
-            </div> */}
-            {/* <div className="mt-4 text-gray-500 grid grid-cols-2">
+                        return (
+                            <button key={index} className={baseButtonClass + buttonClass + " group " + `${(index === selectedCount) || (isDropdown)? 'block': 'hidden'} lg:block`} onClick={() => {handleClick(index); handleDropdown(isDropdown);}}>
+                                {plist.name}<span className={"ml-1 group-hover:rotate-180 " + `${(index == selectedCount) ? 'inline-block': 'hidden'} lg:hidden`}><MdKeyboardArrowDown /></span>
+                            </button>
+                        );
+                    })}
+                </div>
+                <div className="mt-4 lg:mt-8 ml-4 h-[200px] min-w-[200px] gap-x-5 gap-y-2 flex-none grid grid-cols-1 lg:grid-cols-2 items-start content-start">
                 {
                     plists[selectedCount].stack.map((st, index) => (
                         <div key={index} className="flex items-center">
-                            <span className="inline-block">{techMap[st]}</span>
+                            <span className="inline-block">{techMap[st as keyof typeof techMap] ?? <GrStatusUnknown />}</span>
                             <span className="ml-2 inline-block">{st}</span>
                         </div>
                     ))
                 }
-            </div> */}
-
-
+            </div>
+            </div>
         </div>
     );
 }
